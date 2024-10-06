0d4146a94ac7cbec9b152484126efe787a0b7bbe
Matthew Chivers <matthewchivers@outlook.com>
2024-10-06 15:26:58 +0000

    added create command

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,23 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
 	"fmt"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "dodl",
 	Short: "dodl is a document creation tool",
-	Long: `dodl is a command-line tool for creating structured documents using templates.`,
+	Long:  `dodl is a command-line tool for creating structured documents using templates.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Welcome to dodl! Run 'dodl help' to get started.")
 	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
 }
 
 func Execute() {

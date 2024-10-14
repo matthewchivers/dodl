1c1b365d20e00dab45cb9eef28af6e4ee44e0abc
Matthew Chivers <matthewchivers@outlook.com>
2024-10-14 20:07:03 +0000

    moved init placeholder to core package

--- core/core.go
+++ core/core.go
@@ -24,6 +24,15 @@ func ExecuteCommand(cmdCtx models.CommandContext) {
 		fmt.Printf("Topic: %s\n", appCtx.cmdCtx.Flags["topic"])
 		fmt.Printf("Date: %s\n", appCtx.cmdCtx.Flags["date"])
 		fmt.Printf("Dry run: %t\n", appCtx.cmdCtx.Flags["dryRun"])
+	case "init":
+		if len(cmdCtx.Args) == 0 {
+			fmt.Println("No directory specified, using current directory.")
+		} else {
+			cmdCtx.Flags["directory"] = cmdCtx.Args[0]
+			fmt.Printf("Directory: %s\n", cmdCtx.Args[0])
+		}
+		fmt.Printf("Directory Flag: %s\n", cmdCtx.Flags["directory"])
+		fmt.Println("Initialising a new workspace...")
 	}
 
 	fmt.Println("Command executed successfully.")
